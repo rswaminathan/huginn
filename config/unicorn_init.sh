8d727835bc201b1646336c2a1b5fda5a92c36cc9
Rahul Swaminathan <rahul+git@swaminathan.com>
2013-03-15 03:48:52 -0700

    pid path

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -13,7 +13,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/deployer/apps/huginn/current
-PID=$APP_ROOT/tmp/pids/unicorn.pid
+PID=/home/deployer/apps/huginn/shared/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=deployer
 set -u
